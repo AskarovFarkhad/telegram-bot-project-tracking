241661a152fe291dd177307464674d8b7e976e45
“Farkhad <askarovfarkhad.96@gmail.com>
2022-09-16 15:40:04 +0300

    changed command

--- src/main/java/com/askarov/bot/telegram/services/command/impl/CreateCommandImpl.java
+++ src/main/java/com/askarov/bot/telegram/services/command/impl/CreateCommandImpl.java
@@ -1,13 +1,19 @@
 package com.askarov.bot.telegram.services.command.impl;
 
 import com.askarov.bot.telegram.entity.Employee;
+import com.askarov.bot.telegram.entity.Project;
+import com.askarov.bot.telegram.enums.StatusProject;
 import com.askarov.bot.telegram.repository.EmployeeRepository;
+import com.askarov.bot.telegram.repository.ProjectRepository;
 import com.askarov.bot.telegram.services.command.CommandConsole;
 import com.askarov.bot.telegram.enums.CallbackData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -16,7 +22,7 @@ public class CreateCommandImpl implements CommandConsole {
     private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public CreateCommandImpl(EmployeeRepository employeeRepository) {
+    public CreateCommandImpl(EmployeeRepository employeeRepository, ProjectRepository projectRepository) {
         this.employeeRepository = employeeRepository;
     }
 
@@ -27,7 +33,6 @@ public class CreateCommandImpl implements CommandConsole {
 
     @Override
     public String execute(Update update) {
-
         Employee employee = Employee.builder()
                 .employeeChatId(update.getCallbackQuery().getFrom().getId())
                 .employeeFirstName(update.getCallbackQuery().getFrom().getFirstName())
--- src/main/java/com/askarov/bot/telegram/services/command/impl/DeleteCommandImpl.java
+++ src/main/java/com/askarov/bot/telegram/services/command/impl/DeleteCommandImpl.java
@@ -1,14 +1,24 @@
 package com.askarov.bot.telegram.services.command.impl;
 
+import com.askarov.bot.telegram.entity.Employee;
+import com.askarov.bot.telegram.repository.EmployeeRepository;
 import com.askarov.bot.telegram.services.command.CommandConsole;
 import com.askarov.bot.telegram.enums.CallbackData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Slf4j
 @Component
 public class DeleteCommandImpl implements CommandConsole {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public DeleteCommandImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @Override
     public String getCommandSyntax() {
@@ -17,6 +27,7 @@ public class DeleteCommandImpl implements CommandConsole {
 
     @Override
     public String execute(Update update) {
+        employeeRepository.deleteById(update.getCallbackQuery().getFrom().getId());
         return "Вы удалены из списка сотрудников!";
     }
 }
